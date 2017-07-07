401716b1b96f78df8b6159536ae2ed9a606e4ebc
Ron Martinez <ramartin.net@gmail.com>
2017-07-07 17:31:50 -0400

    Remove some original project-specific code

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,28 +1,7 @@
 <template>
   <div>
-    <h1>YESLint</h1>
+    <h1>APP</h1>
     <router-view></router-view>
-    <!--
-    <pre>
-      {{ JSON.stringify(rules, undefined, 2) }}
-    </pre>
-    <table>
-      <thead>
-        <tr>
-          <th>Rule:</th>
-          <th>Description</th>
-          <th>Category</th>
-          <th>Recommended</th>
-          <th>Fixable</th>
-          <th>Deprecated</th>
-          <th>Schema</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr is="app-rule" v-for="(rule, name) in rules" :name="name" :rule="rule"></tr>
-      </tbody>
-    </table>
-    -->
   </div>
 </template>
 
@@ -31,14 +10,12 @@
   "use strict"
 
   import {} from "./main.css"
-  import rules from "../generated/rules"
 
   export default {
-    data() {
-      return {
-        rules: rules,
-      }
-    },
+    // data() {
+    //   return {
+    //   }
+    // },
     created() {
       this.$router.push("/")
     },
