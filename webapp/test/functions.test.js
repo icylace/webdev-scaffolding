fcb92d74b11893b907851d7c4fb14ee8f58be2f9
Ron Martinez <ramartin.net@gmail.com>
2017-07-07 17:34:47 -0400

    Tweak unit testing scaffold

--- webapp/test/functions.test.js
+++ webapp/test/functions.test.js
@@ -5,16 +5,17 @@ import test from "ava"
 import { foo } from "../lib/functions.js"
 
 test("should run", (t) => {
-  const x = true
-  const y = false
   t.pass()
-  t.fail()
-  t.truthy(x)
-  t.falsy(x)
-  t.true(x)
-  t.false(x)
-  t.is(x, y)
-  t.not(x, y)
-  t.deepEqual(x, y)
-  t.notDeepEqual(x, y)
+  // const x = true
+  // const y = false
+  // t.pass()
+  // t.fail()
+  // t.truthy(x)
+  // t.falsy(x)
+  // t.true(x)
+  // t.false(x)
+  // t.is(x, y)
+  // t.not(x, y)
+  // t.deepEqual(x, y)
+  // t.notDeepEqual(x, y)
 })
