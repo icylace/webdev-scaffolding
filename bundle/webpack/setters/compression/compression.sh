90cecf973cc8aba2ac1bcfa5c99af8546259723c
Ron Martinez <ramartin.net@gmail.com>
2020-04-17 20:02:22 -0400

    wip

--- bundle/webpack/setters/compression/compression.sh
+++ bundle/webpack/setters/compression/compression.sh
@@ -7,31 +7,31 @@ setup_webpack_compression() {
   # Prepare Brotli-compressed versions of assets to serve them
   # with Content-Encoding: br
   # https://github.com/mynameiswhm/brotli-webpack-plugin
-  # https://www.npmjs.com/package/
+  # https://www.npmjs.com/package/brotli-webpack-plugin
   modules+=('brotli-webpack-plugin')
 
   # compression-webpack-plugin
   # Prepare compressed versions of assets to serve them with Content-Encoding
   # https://github.com/webpack-contrib/compression-webpack-plugin
-  # https://www.npmjs.com/package/
+  # https://www.npmjs.com/package/compression-webpack-plugin
   modules+=('compression-webpack-plugin')
 
   # Imagemin Webpack
   # Webpack loader and plugin to compress images using imagemin
   # https://github.com/itgalaxy/imagemin-webpack
-  # https://www.npmjs.com/package/
+  # https://www.npmjs.com/package/imagemin-webpack
   modules+=('imagemin-webpack')
 
   # imagemin-jpegtran
   # jpegtran plugin for imagemin
   # https://github.com/imagemin/imagemin-jpegtran
-  # https://www.npmjs.com/package/
+  # https://www.npmjs.com/package/imagemin-jpegtran
   modules+=('imagemin-jpegtran')
 
   # imagemin-optipng
   # Imagemin plugin for OptiPNG
   # https://github.com/imagemin/imagemin-optipng
-  # https://www.npmjs.com/package/
+  # https://www.npmjs.com/package/imagemin-optipng
   modules+=('imagemin-optipng')
 
   npm install --save-dev "${modules[@]}"
